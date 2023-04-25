fe9161d1a2a454f6230e8078920e19f83328e2b8
Binecomestogetyou <cicero1995augustoo@gmail.com>
2023-04-24 22:46:04 -0300

    Score View implementada

--- app/src/main/java/com/bine/pokejuizo/Score.kt
+++ app/src/main/java/com/bine/pokejuizo/Score.kt
@@ -1,19 +1,16 @@
 package com.bine.pokejuizo
 
 import android.content.Context
-import android.text.Editable
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.RatingBar
-import android.widget.TextView
-import com.bine.pokejuizo.R.styleable.Score_stepSize
+import android.widget.*
+import java.util.*
 
-class Score @JvmOverloads constructor (ctx : Context, attributeSet: AttributeSet? = null) : LinearLayout(ctx, attributeSet){
+class Score @JvmOverloads constructor (ctx : Context, attributeSet: AttributeSet? = null, defStyle : Int = 0) : LinearLayout(ctx, attributeSet, defStyle){
 
     var textView : TextView
-    private var score : EditText
+
+    var buttons : LinkedList<ToggleButton> = LinkedList()
 
     var text : String
     get() { return textView.text.toString() }
@@ -23,17 +20,52 @@ class Score @JvmOverloads constructor (ctx : Context, attributeSet: AttributeSet
 
         (ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.score_view, this)
 
-        textView = findViewById(R.id.SV_TV)
+        this.orientation = VERTICAL
 
-        score = findViewById(R.id.SV_ET)
+        textView = findViewById(R.id.SV_TV)
 
         if(attributeSet != null) {
 
             val attr = ctx.obtainStyledAttributes(attributeSet, R.styleable.Score)
 
-            text = attr.getString(R.styleable.Score_text)!!
+            text = attr.getString(R.styleable.Score_text).toString()
+
+            val linearLayout = findViewById<LinearLayout>(R.id.SV_LL)
+
+            var maxButtons = attr.getInt(R.styleable.Score_max, 1)
+
+            val minButtons = attr
+
+            do {
+                val switch = ToggleButton(ctx)
+
+                switch.setOnClickListener { switch -> onClick(switch as ToggleButton) }
+
+                buttons.add(switch)
+
+                linearLayout.addView(switch)
+
+                maxButtons--
+            }while (maxButtons > 0)
 
             attr.recycle()
+        }
+    }
+
+    fun onClick(toggleButton: ToggleButton){
+
+        if(toggleButton.isChecked){
+            for(b in buttons){
+
+                if(buttons.indexOf(b) < buttons.indexOf(toggleButton)) b.isChecked = true
+
+                if(buttons.indexOf(b) > buttons.indexOf(toggleButton)) b.isChecked = false
+            }
+        }
+        else{
+            for(b in buttons){
+                if(buttons.indexOf(b) > buttons.indexOf(toggleButton)) b.isChecked = false
+            }
         }
     }
 }
